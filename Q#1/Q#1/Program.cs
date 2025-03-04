700adb64696a31668cf544bf4380a1ce771735b4
Hafiz10119 <umarbinsaleem10119@gmail.com>
2025-03-04 23:20:33 +0500

    Aur G ki hal ne !?

---
+++ Q#1/Q#1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_P1_Q1
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public double Price { get; set; }
+
+        public void DisplayBookInfo()
+        {
+            Console.WriteLine("Book Details : ");
+            Console.WriteLine($"Book Title : {Title}");
+            Console.WriteLine($"Book Author : {Author}");
+            Console.WriteLine($"Book ISBN : {ISBN}");
+            Console.WriteLine($"Book Price : {Price:F2}");
+
+        }
+        public void ApplyDiscount(double percentage)
+        {
+            if (percentage < 0 || percentage > 100)
+            {
+                Console.WriteLine("Invalid Discount amount, PLease enter between 0 and 100!");
+            }
+            double discountAmount = (Price * percentage) / 100;
+            Price -= discountAmount;
+            Console.WriteLine($"Discount of {percentage}% applied, New price is : {Price}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.Title = "Murder of History";
+            book1.Author = "KK Aziz";
+            book1.ISBN = "789564523";
+            book1.Price = 999;
+            book1.DisplayBookInfo();
+            book1.ApplyDiscount(10);
+            book1.DisplayBookInfo();
+
+            Console.WriteLine("\t---***----");
+
+            Book book2 = new Book();
+            book2.Title = "Mushaf";
+            book2.Author = "Nimra Ahmed";
+            book2.ISBN = "88749654263";
+            book2.Price = 1499;
+            book2.DisplayBookInfo();
+            book2.ApplyDiscount(10);
+            book2.DisplayBookInfo();
+
+
+        }
+    }
+}
---
+++ Q#1/Q#2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_2
+{
+    class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int employeeID, string name, string department, double salary)
+        {
+            EmployeeID = employeeID;
+            Name = name;
+            Department = department;
+            Salary = salary;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"-----***Emplyee Details***-----");
+            Console.WriteLine($"Emplyee ID : {EmployeeID}");
+            Console.WriteLine($"Employee Name : {Name}");
+            Console.WriteLine($"Employee Department : {Department}");
+            Console.WriteLine($"Employee Salary : {Salary}");
+
+        }
+        public void IncreaseSalary(double amount)
+        {
+            Salary = Salary + amount;
+            Console.WriteLine($"Salary increased by {amount:F2}, New salary amount is {Salary:F2}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(4869, "Umar", "DCS", 1000);
+            emp.DisplayEmployeeDetails();
+            emp.IncreaseSalary(300);
+            Console.WriteLine("----**----");
+            Console.WriteLine("After Update Information is :");
+            emp.DisplayEmployeeDetails();
+        }
+    }
+}
